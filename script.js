f5791af3dba8e53d97e6acb3a8446c0a2bed9b02
Viper <lmi25@txstate.edu>
2026-01-23 22:50:57 -0600

    Fixed the interactivity of the share-button on mobile and desktop resolutions.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var dialog = document.querySelector("dialog");
-var showButton = document.querySelector("dialog button")
-var closeButton = document.querySelector("dialog button");
+var showButton = document.querySelector(".share-button");
+var closeButton = document.querySelector(".pop-up-button");
 
 showButton.addEventListener('click', () => {
     dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener('click', () => {
     dialog.close();
 });
 
